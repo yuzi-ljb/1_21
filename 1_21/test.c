31a04b97ddb27b5203fe0cc4933014c17383a10a
佳斌 李 <l3234216896@gmail.com>
2025-01-21 20:42:59 +0800

    到算术操作符

---
+++ 1_21/test.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int c = 1500;
+int main()
+{
+	/*_Bool b = true;
+	if(b)
+	printf("ljb\n");*/
+	
+	/*int a;
+	short int b;
+	long int c;
+
+	float d;			//数据类型 + 变量名 (创建变量)
+	double f;
+	long double g;
+
+	char h;
+
+	_Bool i;
+	
+	*/
+
+	/*int c = 1000;
+
+	printf("c=%dn", c);*/
+
+	/*int a = 100 + 500;
+	int b = 500 - 100;
+	printf("a=%d\n", a);	
+	printf("b=%d\n", b);*/
+	
+	/*int c = 5;
+	printf("c=%d\n", c * c * c);*/
+
+
+
+	float a = 5.0 / 2;
+	float b = 5 / 2;
+	printf("a=%f\n", a);
+	printf("b=%f\n", b);
+	return 0;
+}
